c63f931ab8f75944905aedda5db94af93b6a92ed
zoggga <98279881+zoggga@users.noreply.github.com>
2022-01-23 19:19:05 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const data = [
+	{
+		_id: 1,
+		body: '123'
+	},
+	{
+		_id: 2,
+		body: '321'
+	},
+];
+
+app.get('/data', (req, res) => {
+	res.send(JSON.stringify(data));
+});
+
+app.listen(8089, () => {
+	console.log('Server started');
+});
